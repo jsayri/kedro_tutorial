e7fe1bc6e298a2386e452af8dec5240e3c0ec0f0
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2022-01-29 14:32:40 +0100

    Extend the data processing pipeline

--- src/kedro_tutorial/pipelines/data_processing/pipeline.py
+++ src/kedro_tutorial/pipelines/data_processing/pipeline.py
@@ -1,7 +1,7 @@
 from kedro.pipeline import Pipeline, node
 
 from .nodes import preprocess_companies, preprocess_shuttles
-
+from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
 
 def create_pipeline(**kwargs):
     return Pipeline(
@@ -17,6 +17,12 @@ def create_pipeline(**kwargs):
                 inputs="shuttles",
                 outputs="preprocessed_shuttles",
                 name="preprocess_shuttles_node",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table_node",
             ),
         ]
     )
